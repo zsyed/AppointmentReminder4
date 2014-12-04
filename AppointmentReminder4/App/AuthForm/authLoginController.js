9384dd626607372687836fc1ef292695880864b1
zsyed <zulfiqarsyed@yahoo.com>
2014-12-04 15:10:32 -0800

    login screen , delay is fixed.

--- AppointmentReminder4/App/AuthForm/authLoginController.js
+++ AppointmentReminder4/App/AuthForm/authLoginController.js
@@ -1,14 +1,26 @@
 ﻿appointmentReminderApp.controller('authLoginController',
 	function AuthLoginFormController($scope, $window, $location, $routeParams, authService) {
+	    $scope.loadinglogin = false;
 	    $scope.loginForm = function () {
+	        $scope.$broadcast('LOGIN_LOADED_EVENT');
 	        authService.loginUser($scope.loginData).then(
                 function (results) {
+
                     $location.path('/home');
                 },
                     function (results) {
                         // on error
                         var data = results.data;
+                        $scope.$broadcast('LOGIN_STOPLOADED_EVENT');
                     }
                 );
 	    };
+
+	    $scope.$on('LOGIN_LOADED_EVENT', function () {
+	        $scope.loadinglogin = true;
+	    });
+
+	    $scope.$on('LOGIN_STOPLOADED_EVENT', function () {
+	        $scope.loadinglogin = false;
+	    });
 	});
