ecf556577404987205fa9c42f425c228916f6332
zsyed <zulfiqarsyed@yahoo.com>
2014-12-04 07:03:52 -0800

    Reminder Contact Controller some how got removed. Added back in again.

---
+++ AppointmentReminder4/Controllers/ReminderContactController.cs
@@ -0,0 +1,36 @@
+﻿using AppointmentReminder.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppointmentReminder4.Controllers
+{
+    public class ReminderContactController : ApiController
+    {
+ 		private IReminderDb _db;
+		public ReminderContactController(IReminderDb db)
+		{
+			_db = db;
+		}
+		public IHttpActionResult GetAllReminderContacts()
+		{
+			var profile = _db.Profiles.ToList().Find(p => p.UserName == User.Identity.Name);
+			var profileContacts = _db.Contacts.Where(c => c.ProfileId == profile.Id).ToList();
+			var contacts = new List<ReminderContact>();
+			foreach (var profileContact in profileContacts)
+			{
+				contacts.Add(new ReminderContact()
+					             {
+						             Id = profileContact.Id, 
+									 name = string.Format("{0} {1}", profileContact.FirstName, profileContact.LastName)
+					             });
+			}
+			return this.Ok(contacts);
+		}
+
+
+	}
+}
