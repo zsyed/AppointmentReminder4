a9f2102e7b2c791fc219f8a3a9cdf0bd6cd0596b
zsyed <zulfiqarsyed@yahoo.com>
2015-02-09 04:15:18 +0000

    upon reset on the browser fix for login, register, and resetpwd

---
+++ AppointmentReminder4/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppointmentReminder4.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            var url = "http://" + Request.Url.Authority + "/#/Login";
+            return Redirect(url);
+        }
+    }
+}
---
+++ AppointmentReminder4/Controllers/ResetPasswordController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppointmentReminder4.Controllers
+{
+    public class ResetPasswordController : Controller
+    {
+        // GET: ResetPassword
+        public ActionResult Index()
+        {
+            var url = "http://" + Request.Url.Authority + "/#/ResetPassword";
+            return Redirect(url);
+        }
+    }
+}
