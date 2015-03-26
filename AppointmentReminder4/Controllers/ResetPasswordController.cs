24d9a0e44e395af2b48cd869bfb0d3ad68e9c020
zsyed <zulfiqarsyed@yahoo.com>
2015-03-26 16:31:03 -0700

    committing to synch

---
+++ AppointmentReminder4/Controllers/ResetPasswordController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppointmentReminder4.Controllers
+{
+    public class ResetPasswordController : Controller
+    {
+        // GET: ResetPassword
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
